3554c2cbfd76682ff1b3a266ac3f07cbd9fad3b9
firmanms <firmanmuhamadsahidin@gmail.com>
2025-03-22 15:12:56 +0700

    220325a

--- resources/views/filament/pages/manualbook.blade.php
+++ resources/views/filament/pages/manualbook.blade.php
@@ -1,3 +1,4 @@
 <x-filament-panels::page>
-<h1>Halaman Kosong</h1>
+<a href="https://docs.google.com/document/d/1jNpPHcQlga-tySKdbNbppPL0UHsbMruRnpvzQZSbcyU/edit?usp=sharing" target="_blank"><button>Lihat</button></a>
+<iframe src="https://docs.google.com/document/d/e/2PACX-1vSMvuAHI1jihr3L57VVMGOnuiUcr-_VERFCjbaEovcqzqFVMeR3tcCxjtioh-uJEoVqzmmR1Qn01TR5/pub?embedded=true" style="width:100%; height:1000px;"></iframe>
 </x-filament-panels::page>
